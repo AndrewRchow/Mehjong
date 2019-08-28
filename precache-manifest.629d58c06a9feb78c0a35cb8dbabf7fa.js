310cd50016562ba6eb4e1939792c552726638824
Andrew Chow <chowlick@gmail.com>
2019-08-28 14:04:27 -0700

    Updates

---
+++ precache-manifest.629d58c06a9feb78c0a35cb8dbabf7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0195fdc8e55c2447b01e0bdc35694b54",
+    "url": "/mehjong/index.html"
+  },
+  {
+    "revision": "147c027b333c1a80b0c0",
+    "url": "/mehjong/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ee6a4e36ad4e2ebc18cc",
+    "url": "/mehjong/static/js/2.ddd97f5c.chunk.js"
+  },
+  {
+    "revision": "147c027b333c1a80b0c0",
+    "url": "/mehjong/static/js/main.9adfac70.chunk.js"
+  },
+  {
+    "revision": "90514742e14fceb2b984",
+    "url": "/mehjong/static/js/runtime~main.44dd9da3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mehjong/static/media/logo.5d5d9eef.svg"
+  }
+]);
